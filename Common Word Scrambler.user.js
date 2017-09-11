efda755f258c48283e414d1efc7b411777b2c813
Gabe <GittyLitter@users.noreply.github.com>
2017-09-11 14:20:35 -0600

    Fixed version #

--- Common Word Scrambler.user.js
+++ Common Word Scrambler.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Common Word Scrambler
-// @version 0.1
+// @version 0.2
 // @updateURL https://github.com/GittyLitter/word-scrambler/raw/master/Common%20Word%20Scrambler.user.js
 // @include *
 // ==/UserScript==
