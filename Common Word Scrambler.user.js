e0a0b15499cec21f23012309805ff0bdad1f8da3
Gabe <GittyLitter@users.noreply.github.com>
2017-09-11 14:17:10 -0600

    Update Common Word Scrambler.user.js
    Added update URL

--- Common Word Scrambler.user.js
+++ Common Word Scrambler.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name Common Word Scrambler
 // @version 0.1
+// @updateURL https://github.com/GittyLitter/word-scrambler/raw/master/Common%20Word%20Scrambler.user.js
 // @include *
 // ==/UserScript==
 
