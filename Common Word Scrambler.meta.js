87b03c39fa16463881d0aa1ed55c3b4182677ca3
Gabe <GittyLitter@users.noreply.github.com>
2017-09-12 10:25:45 -0600

    Create Common Word Scrambler.meta.js

---
+++ Common Word Scrambler.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name Common Word Scrambler
+// @version 0.3
+// @updateURL https://www.github.com/GittyLitter/word-scrambler/raw/master/Common%20Word%20Scrambler.user.js
+// @include *
+// ==/UserScript==
