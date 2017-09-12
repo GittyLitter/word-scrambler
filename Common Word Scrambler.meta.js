2b88f6c3be10505a26b645546154ed53c58b4e9d
Gabe <GittyLitter@users.noreply.github.com>
2017-09-12 10:39:41 -0600

    Update Common Word Scrambler.meta.js

--- Common Word Scrambler.meta.js
+++ Common Word Scrambler.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Common Word Scrambler
-// @version 0.3
+// @version 0.5
 // @updateURL https://www.github.com/GittyLitter/word-scrambler/raw/master/Common%20Word%20Scrambler.user.js
 // @downloadURL https://www.github.com/GittyLitter/word-scrambler/raw/master/Common%20Word%20Scrambler.meta.js
 // @include *
